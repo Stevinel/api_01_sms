776ad63abac5f19504ee013c2f07526875b7d52e
Станислав <stevinel@xaker.ru>
2020-12-22 22:21:27 +0300

    hw sms v1

--- homework.py
+++ homework.py
@@ -1,27 +1,42 @@
+import os
 import time
-import requests
 
+import requests
+from dotenv import load_dotenv
 from twilio.rest import Client
+
+load_dotenv()
 
 
 def get_status(user_id):
     params = {
-        ...
+        "user_ids": user_id,
+        "fields": "online",
+        "v": 5.92,
+        "access_token": os.environ["VK_TOKEN"],
     }
-    ...
-    return ...  # Верните статус пользователя в ВК
+    response = requests.post(
+        url="https://api.vk.com/method/users.get", params=params
+    )
+    return response.json()["response"][0]["online"]
 
 
-def send_sms(sms_text, client):
-    ...
-    return ...  # Верните sid отправленного сообщения из Twilio
+def sms_sender(sms_text):
+    account_sid = os.environ["ID"]
+    auth_token = os.environ["TOKEN"]
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body=sms_text,
+        from_=os.getenv("NUMBER_FROM"),
+        to=os.getenv("NUMBER_TO"),
+    )
+    return message.sid
 
 
-if __name__ == '__main__':
-    # тут происходит инициализация Client
-    vk_id = input('Введите id ')
+if __name__ == "__main__":
+    vk_id = input("Введите id ")
     while True:
         if get_status(vk_id) == 1:
-            send_sms(f'{vk_id} сейчас онлайн!')
+            sms_sender(f"{vk_id} сейчас онлайн!")
             break
         time.sleep(5)
